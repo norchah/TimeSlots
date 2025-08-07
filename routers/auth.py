0c9067b16d83ffe101bca310b47ebada57dcacee
norchah <norchah@mail.ru>
2025-08-08 01:56:44 +0200

    added endpont /me

--- routers/auth.py
+++ routers/auth.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import Session
 
 from db import get_db
 from models import UserDB
-from schemas import LoginRequest, TokenResponse
-from utils.auth import create_access_token
+from schemas import LoginRequest, UserResponse, RoleResponse
+from utils.auth import create_access_token, get_current_user
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -31,10 +31,24 @@ def login(request: LoginRequest, response: Response, db: Session = Depends(get_d
         samesite="lax",  # Защита от CSRF
         max_age=30 * 60  # 30 минут
     )
+    return {"user": user.id}
 
-    return TokenResponse(token_type="bearer", roles=roles)
 
 @auth_router.post("/logout")
 def logout(response: Response):
     response.delete_cookie(key="access_token")
     return {"status": "logged out"}
+
+
+@auth_router.get("/me", response_model=UserResponse)
+def get_me(current_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+    user_id = current_user["user_id"]
+    user = db.query(UserDB).filter(UserDB.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return UserResponse(
+        id=user.id,
+        username=user.username,
+        roles=[RoleResponse(id=r.id, name=r.name, description=r.description) for r in user.roles]
+    )
